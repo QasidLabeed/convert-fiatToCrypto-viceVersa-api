e970d4b68f3ec4ac422a0a9e0173f28b857338cd
Qasid Labeed <qasidlabeed@gmail.com>
2017-09-24 18:25:43 +0500

    API final

--- Restful API for conversion/Controllers/ConverterController.cs
+++ Restful API for conversion/Controllers/ConverterController.cs
@@ -1,16 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net.Http;
+using System.Text;
 using System.Web.Http;
+using convert_fiatToCrypto_viceVersa_api;
+
 
 namespace Restful_API_for_conversion.Controllers
 {
     public class ConverterController : ApiController
     {
-        string abc;
-        re
+
+
+        public HttpResponseMessage GET()
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.Content = new StringContent("The api format is\n localhost:8548/api/converter?from=value&to=value&amount=value", Encoding.Unicode, "application/json");
+            return response;
+
+        }
+        public HttpResponseMessage GET(string from,string to,int amount)
+        {
+            CurrencyConversion currencyConversion = new CurrencyConversion();
+            string result =currencyConversion.CallApi(from,to,amount);
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.Content = new StringContent(result, Encoding.Unicode, "application/json");
+            return response;
+
+        }
         
     }
 }
--- convert-fiatToCrypto-viceVersa-api/CurrencyConversion.cs
+++ convert-fiatToCrypto-viceVersa-api/CurrencyConversion.cs
@@ -6,26 +6,34 @@ using System.Threading.Tasks;
 
 namespace convert_fiatToCrypto_viceVersa_api
 {
-    class CurrencyConversion
+    public class CurrencyConversion
     {
+        // No Parameter function
         public string CallApi()
         {
-            //Get Currency to convert to
-            Console.WriteLine("Enter Currecy From");
-            string from = Console.ReadLine();
-
-            //Get Currency to be converted
-            Console.WriteLine("Enter Currency To");
-            string to = Console.ReadLine();
-
-            //Get the amount to be converted
-            Console.WriteLine("Enter Amount to be converted ");
-            double amount = Convert.ToDouble(Console.ReadLine());
-
-
-            string[] Param = { from, to };
-            string conv = "https://min-api.cryptocompare.com/data/pricehistorical?fsym=" + Param[0] + "&tsyms=" + Param[1];
             
+            //string[] Param = { from, to };
+            string conv = "https://min-api.cryptocompare.com/data/pricehistorical?fsym=USD&tsyms=AUD";
+            
+            //Object creation
+            Api ConvCurrency = new Api();
+            var response = ConvCurrency.CallApi(conv);
+
+            //Convert string result to JSON Object
+            var JObj = ConvCurrency.GetJsonObj(response);
+
+            return Convert.ToString(JObj["USD"]);
+        }
+
+        //Overloaded function
+        public string CallApi(string from, string to, int amount)
+        {
+           
+
+
+            string[] Param = { from,to };
+            string conv = "https://min-api.cryptocompare.com/data/pricehistorical?fsym=" + Param[0] + "&tsyms=" + Param[1];
+
             //Object creation
             Api ConvCurrency = new Api();
             var response = ConvCurrency.CallApi(conv);
@@ -36,7 +44,8 @@ namespace convert_fiatToCrypto_viceVersa_api
             //Value extracted from JObj is converted to double for calculation
             var result = Convert.ToDouble(JObj[from][to]);
             var finalAmount = result * amount;
-            return "1 " + from + " = " +result+" "+to+ "\n"+ "So "+amount+" "+from+" = "+finalAmount+" "+to;
-        }    
+           
+            return Convert.ToString(finalAmount);
+        }
     }
 }
